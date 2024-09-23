f32d97ee9f095baa7b32df80d688da49d60d34aa
John Dave Cadiz <johndave.cadiz@students.isatu.edu.ph>
2024-09-23 14:33:10 +0000

    Recommitting

--- Lab_Activities/RWorksheet_cadiz#2.R
+++ Lab_Activities/RWorksheet_cadiz#2.R
@@ -2,4 +2,24 @@
 
 ## 1. Create a vector using : operator
 
-- a. Sequence from -5 to 5. Write the R code and its output. Describe its output.
+#a. Sequence from -5 to 5. Write the R code and its output. Describe its output.
+num_vector <- -5:5
+print(num_vector)
+#In this code the sequence will start from the negative five to the positive five, 
+#with the help of the ";" operator it states that the origin will start at
+#negative five and will end to positive five.
+
+#b. x <- 1:7. What will be the value of x?
+#The value of the x will be 1, 2, 3, 4, 5, 6, 7.
+
+## 2. Create a vector using seq() function
+
+#a. seq(1,3, by=0.2) #specify step size
+#Write the R script and its output. Describe the output.
+nseq <- seq(1,3, by=0.2)
+print(nseq)
+# The output shows that the it will start to number one and it will increment by 
+#0.2 until it reaches to number 3.
+
+
+
